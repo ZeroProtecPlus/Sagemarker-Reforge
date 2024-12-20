f498bd106dc5d968fa28a1310f4bd4f308e03921
Juan Sebastian Giraldo <juangiraldo.sc@academia.umb.edu.co>
2024-12-20 18:53:00 -0500

    Added aria2 installation and fixed the path links.txt

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "Downloading, this might take a while..."
-aria2c aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --continue --input-file=~/Sagemarker-Reforge/links.txt
+pip install aria2
+aria2c aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --input-file=/home/studio-lab-user/Sagemarker-Reforge/links.txt
 
-if [ ! -f libtcmalloc_minimal.so.4 ]; then
-    aria2c -o libtcmalloc_minimal.so.4 \
-    https://github.com/camenduru/gperftools/releases/download/v1.0/libtcmalloc_minimal.so.4
-else
-    echo "libtcmalloc_minimal.so.4 already exists, skipping download."
-fi
+# if [ ! -f libtcmalloc_minimal.so.4 ]; then
+#     aria2c -o libtcmalloc_minimal.so.4 \
+#     https://github.com/camenduru/gperftools/releases/download/v1.0/libtcmalloc_minimal.so.4
+# else
+#     echo "libtcmalloc_minimal.so.4 already exists, skipping download."
+# fi
